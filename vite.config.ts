ecbf050bc31d3ecc2528355fcd332c0226d25c38
murilocicolin <mu_cicolin@hotmail.com>
2025-07-24 00:59:49 -0300

    oks

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/amor-perfeito-doce-lucrativo/" : "/",
+  base: "./",
 
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
